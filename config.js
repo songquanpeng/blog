4d6524121e6a6d139b5f23b6b28a0f7abd00109a
Song <quanpengsong@gmail.com>
2019-10-25 21:49:55 +0800

    Now you can config the ad and statistics code

--- config.js
+++ config.js
@@ -4,6 +4,31 @@ const CDN = {
   cloudflare: 'cdnjs.cloudflare.com' // for other users
 };
 
+const externalStatisticsCode = `    
+    <script>
+      var _hmt = _hmt || [];
+      (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?61fbf466b30ee950df155c38b719b1f3";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+    </script>
+`;
+const articleAd = `
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+             style="display:block; text-align:center;"
+             data-ad-layout="in-article"
+             data-ad-format="fluid"
+             data-ad-client="ca-pub-4932639067711253"
+             data-ad-slot="4180812612"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            overlays: {bottom:true}
+          });
+        </script>`;
+
 config = {
   domain: 'iamazing.cn',
   owner: 'JustSong',
@@ -12,7 +37,9 @@ config = {
   cdn: CDN.staticfile,
   motto: '尽人事，听天命',
   link: 'https://github.com/songwonderful',
-  enableComment: true
+  enableComment: true,
+  externalStatisticsCode: externalStatisticsCode, // This code will be insert into <head>
+  articleAd: articleAd // This ad will show at the bottom of article
 };
 
 module.exports = {
