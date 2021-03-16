0f13e98601331db38b260fbac494c66cb0837652
Song <quanpengsong@gmail.com>
2021-03-17 00:08:44 +0800

    :tada: database migratation script done

---
+++ common/migrate.js
@@ -0,0 +1,74 @@
+const sqlite3 = require('sqlite3');
+const oldDb = new sqlite3.Database('./old.db');
+const { initializeDatabase, Page, User, Option } = require('../models');
+
+async function getAdminId() {
+  let user = await User.findOne({
+    where: {
+      username: 'admin'
+    },
+    raw: true
+  });
+  return user.id;
+}
+
+async function migratePages() {
+  let id = await getAdminId();
+  oldDb.all('select * from pages', [], (err, pages) => {
+    if (err) {
+      console.error(err);
+    }
+    pages.forEach(async page => {
+      try {
+        await Page.create({
+          type: page.type,
+          link: page.link,
+          pageStatus: page.page_status,
+          commentStatus: page.comment_statis,
+          title: page.title,
+          content: page.content,
+          tag: page.tag,
+          description: page.description,
+          password: page.password,
+          view: page.view,
+          upVote: page.up_vote,
+          downVote: page.down_vote,
+          UserId: id,
+          createdAt: page.post_time,
+          updatedAt: page.edit_time
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  });
+}
+
+async function migrateOptions() {
+  oldDb.all('select * from options', [], (err, options) => {
+    if (err) {
+      console.error(err);
+    }
+    options.forEach(async option => {
+      try {
+        let optionObj = await Option.findOne({
+          where: {
+            key: option.name
+          }
+        });
+        await optionObj.update({
+          key: option.name,
+          value: option.value
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  });
+}
+
+(async () => {
+  await initializeDatabase();
+  await migratePages();
+  await migrateOptions();
+})();
