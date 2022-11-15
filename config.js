ae7f65365b4cec77fbfc1601a2bd7c000f88a77a
JustSong <quanpengsong@gmail.com>
2022-11-15 11:53:24 +0800

    :tada: use random secret

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let config = {
   debug: true,
   port: process.env.PORT || 3000,
   database: 'data.db',
-  session_secret: 'justice',
+  session_secret: Math.random().toString(),
   auth_cookie_name: 'blog',
   uploadPath: './public/upload',
   systemName: 'Blog',
